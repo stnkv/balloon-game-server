2d8304094074837579c448295aa5facbf86b503c
stnkv <jacobsitnikov93@gmail.com>
2021-03-11 12:15:37 +0500

    Добавил тестовый рест контроллер

--- src/main/java/ru/stnkv/balloongame/Application.java
+++ src/main/java/ru/stnkv/balloongame/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BalloonGameServerApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BalloonGameServerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
