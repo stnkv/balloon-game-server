3871bc59a0407da3a7e5ae11dc2863b24331babd
stnkv <jacobsitnikov93@gmail.com>
2021-03-12 00:45:01 +0500

    cделал тест для проверки того что endpoint работает

--- src/test/java/ru/stnkv/balloongame/HelloControllerIntegrationTest.java
+++ src/test/java/ru/stnkv/balloongame/HelloControllerIntegrationTest.java
@@ -1,30 +1,28 @@
 package ru.stnkv.balloongame;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 class HelloControllerIntegrationTest {
     @Autowired
-    private MockMvc mvc;
+    private MockMvc mockMvc;
 
     @Test
-    public void givenEmployees_whenGetEmployees_thenStatus200() throws Exception {
-        mvc.perform(get("/")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+    public void shouldReturnWelcomeString() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Welcome !")));
     }
 }
