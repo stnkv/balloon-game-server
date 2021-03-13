9b80d0258869518042094232d94c8891bd8f5c65
stnkv <jacobsitnikov93@gmail.com>
2021-03-13 16:22:58 +0500

    набросал интерфейс итерактора игры

--- src/main/java/ru/stnkv/balloongame/domain/game/IGameInteractor.java
+++ src/main/java/ru/stnkv/balloongame/domain/game/IGameInteractor.java
@@ -5,4 +5,15 @@ package ru.stnkv.balloongame.domain.game;
  * @since 13.03.2021
  */
 public interface IGameInteractor {
+
+    String createGame(String roomId);
+
+    void sendStartGameEvent();
+
+    void sendEventPartitions();
+
+    void sendEndGameEvent();
+
+    void deleteGame(String roomId);
+
 }
